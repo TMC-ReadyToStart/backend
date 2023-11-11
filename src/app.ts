62aaaf6096e7a7a73bdd8b145114b5882075f794
noesusset <noe.susset@epita.fr>
2023-11-11 09:21:46 +0100

    Add CORS package and update port number

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,18 @@ import express from "express";
 import { router as moocRoutes } from "./routes/moocsRoutes";
 import { router as uploadRoutes } from "./routes/uploadRoutes";
 import { router as usersRoutes } from "./routes/usersRoutes";
+import cors from "cors";
 
 export const app = express();
-const port = 3000;
+const port = 3033;
 
 const routes = {
   "/moocs": moocRoutes,
   "/upload": uploadRoutes,
   "/users": usersRoutes,
 };
+
+app.use(cors());
 
 for (const [path, route] of Object.entries(routes)) {
   app.use(path, route);
