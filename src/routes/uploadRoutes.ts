f25b39a4cce96f72dbf9e90a3997d82dc2fcb27d
noesusset <noe.susset@epita.fr>
2023-11-11 07:38:48 +0100

    Add support for uploading and extracting zip files

--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -1,8 +1,29 @@
 import express from "express";
+import bodyParser from "body-parser";
+import AdmZip from "adm-zip";
+import e from "express";
 
 export const router = express.Router();
 
-router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("File uploaded");
+router.use(bodyParser.raw({ type: "application/zip", limit: "1mb" })); // Content-Type must be set to application/zip
+
+router.post("/", (req, res, next) => {
+  if (req.is("application/zip")) {
+    try {
+      const zip = new AdmZip(req.body);
+      const zipEntries = zip.getEntries();
+
+      zipEntries.forEach((entry) => {
+        zip.extractEntryTo(entry.entryName, "./uploads", false, true);
+        // Do something with the entry data (entry.getData())
+      });
+
+      res.send("Successfully extracted zip file");
+    } catch (error) {
+      console.error("Error extracting zip file:", error);
+      res.status(500).send("Error extracting zip file");
+    }
+  } else {
+    res.status(400).send("Content-Type must be application/zip");
+  }
 });
